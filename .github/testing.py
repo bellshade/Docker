196a162dfe2b57776d7c09968baa75edb38523d6
rulanugrh <nextarituenak01@gmail.com>
2024-02-02 22:28:51 +0700

    fix: unittest in python section

--- .github/testing.py
+++ .github/testing.py
@@ -4,7 +4,7 @@ import unittest
 
 class Test(unittest.TestCase):
     def setUp(self):
-        subprocess.check_call(['docker', 'build', '-f', '../example/hello-bellshade/Dockerfile', '-t', 'testing-image', '.'])
+        subprocess.check_call(['docker', 'build', '-f', './example/hello-bellshade/Dockerfile', '-t', 'testing-image', '.'])
     
     def test_image_hello(self):
         output = subprocess.check_output(['docker', 'run', 'testing-image']).decode().strip()
