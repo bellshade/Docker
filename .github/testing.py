7a71d0e79d88ff69228dc52f9a8c503dc9ba1950
rulanugrh <nextarituenak01@gmail.com>
2024-02-02 22:26:53 +0700

    fix: unittest in python sectio

--- .github/testing.py
+++ .github/testing.py
@@ -3,13 +3,12 @@ import subprocess
 import unittest
 
 class Test(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         subprocess.check_call(['docker', 'build', '-f', '../example/hello-bellshade/Dockerfile', '-t', 'testing-image', '.'])
-        docker_id = subprocess.check_output(['docker', 'run', '-d', 'testing-image']).decode().strip()
-        self.host = testinfra.get_host("docker://" + docker_id)
     
     def test_image_hello(self):
-        self.assertEqual(self.host.system_info.type, 'alpine')
+        output = subprocess.check_output(['docker', 'run', 'testing-image']).decode().strip()
+        self.assertEqual(output, "Hello Bellshade")
 
 if __name__ == "__main__":
     unittest.main()
